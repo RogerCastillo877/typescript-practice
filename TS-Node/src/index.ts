9dc3506214f2e1cd3af1a97796ec777b54a2eefa
RogerCastillo877 <roger.castillo877@gmail.com>
2022-02-09 23:08:33 -0500

    Working with webpack

---
+++ TS-Node/src/index.ts
@@ -0,0 +1,14 @@
+import { Hero, Hero2 as Loco } from "./class/Hero";
+const Hero2 = 123;
+
+const ironman = new Hero('Tonny', 15, 20);
+
+// import * as HeroClass from "./class/Hero";
+// import powers from "./data/powers";
+
+// const ironman = new HeroClass.Hero('Tonny', 15, 20);
+
+console.log(ironman);
+
+// console.log( powers );
+
